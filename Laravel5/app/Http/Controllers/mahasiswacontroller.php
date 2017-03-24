6c7c3e42f6556ca0d86915cc6bfec5726afd2b1f
MRizkyWahyudianto <rizkykiwa@gmail.com>
2017-03-24 11:13:08 +0800

    Tugas 4 Done

--- Laravel5/app/Http/Controllers/mahasiswacontroller.php
+++ Laravel5/app/Http/Controllers/mahasiswacontroller.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use App\mahasiswa;
 
-class mahasiswacontroller extends Controller
+use App\Mahasiswa;
+
+class MahasiswaController extends Controller
 {
     public function awal(){
     	return "Hello dari MahasiswaController";
@@ -16,11 +17,11 @@ class mahasiswacontroller extends Controller
     	return $this->simpan();
     }
     public function simpan(){
-    	$mahasiswa = new mahasiswa();
-    	$mahasiswa->nama = "Mochammad Rizky Wahyudianto";
+    	$mahasiswa = new Mahasiswa();
+    	$mahasiswa->nama = "Sugiono";
     	$mahasiswa->nim = "1515015055";
-    	$mahasiswa->alamat = "Jl pattimura";
-    	$mahasiswa->pengguna_id = 1;
+    	$mahasiswa->alamat = "Pemuda";
+    	$mahasiswa->pengguna_id = 3;
     	$mahasiswa->save();
     	return "Data Mahasiswa dengan Nama {$mahasiswa->nama} telah disimpan";
 }
--- Laravel5/app/Http/routes.php
+++ Laravel5/app/Http/routes.php
@@ -2,10 +2,10 @@
 
 /*
 |--------------------------------------------------------------------------
-| Routes File
+| Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you will register all of the routes in an application.
+| Here is where you can register all of the routes for an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
@@ -15,41 +15,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
+Route::get('pengguna', 'PenggunaController@awal');
+Route::get('pengguna/tambah', 'PenggunaController@tambah');
+Route::get('pengguna/{pengguna}','PenggunaController@lihat');
+Route::post('pengguna/simpan','PenggunaController@simpan');
+Route::get('pengguna/edit/{pengguna}','PenggunaController@edit');
+Route::post('pengguna/edit/{pengguna}','PenggunaController@update');
+Route::get('pengguna/hapus/{pengguna}','PenggunaController@hapus');
 
-Route::get('Hello-world', function () {
-    return 'Hello-world';
-});
-/*Route::get('pengguna/{pengguna}', function ($pengguna) {
-    return "Hallo world dari pengguna $pengguna";
-});
-Route::get('berita/{berita?}', function ($berita="Laravel 5") {
-    return "berita $berita belum di baca";
-});*/
-Route::get('pengguna','penggunacontroller@awal');
-Route::get('pengguna/tambah','penggunacontroller@tambah');
-Route::get('dosen', 'dosencontroller@awal');
-Route::get('dosen/tambah', 'dosencontroller@tambah');
-Route::get('mahasiswa', 'mahasiswacontroller@awal');
-Route::get('mahasiswa/tambah', 'mahasiswacontroller@tambah');
+Route::get('Dosen', 'DosenController@awal');
+Route::get('Dosen/tambah', 'DosenController@tambah');
+Route::get('Mahasiswa', 'MahasiswaController@awal');
+Route::get('Mahasiswa/tambah', 'MahasiswaController@tambah');
 
 Route::get('ruangan', 'ruangancontroller@awal');
 Route::get('ruangan/tambah', 'ruangancontroller@tambah');
+Route::get('ruangan/{ruangan}','ruangancontroller@lihat');
+Route::post('ruangan/simpan','ruangancontroller@simpan');
+Route::get('ruangan/edit/{ruangan}','ruangancontroller@edit');
+Route::post('ruangan/edit/{ruangan}','ruangancontroller@update');
+Route::get('ruangan/hapus/{ruangan}','ruangancontroller@hapus');
 
 Route::get('matakuliah', 'matakuliahcontroller@awal');
 Route::get('matakuliah/tambah', 'matakuliahcontroller@tambah');
+Route::get('matakuliah/{matakuliah}','matakuliahcontroller@lihat');
+Route::post('matakuliah/simpan','matakuliahcontroller@simpan');
+Route::get('matakuliah/edit/{matakuliah}','matakuliahcontroller@edit');
+Route::post('matakuliah/edit/{matakuliah}','matakuliahcontroller@update');
+Route::get('matakuliah/hapus/{matakuliah}','matakuliahcontroller@hapus');
 
-Route::get('dosen_matakuliah', 'dosen_matakuliahcontroller@awal');
-Route::get('dosen_matakuliah/tambah', 'dosen_matakuliahcontroller@tambah');
+Route::get('Dosen_matakuliah', 'Dosen_matakuliahController@awal');
+Route::get('Dosen_matakuliah/tambah', 'Dosen_matakuliahController@tambah');
 
-Route::get('jadwal_matakuliah', 'jadwal_matakuliahcontroller@awal');
-Route::get('jadwal_matakuliah/tambah', 'jadwal_matakuliahcontroller@tambah');
+Route::get('Jadwal_matakuliah', 'Jadwal_matakuliahController@awal');
+Route::get('Jadwal_matakuliah/tambah', 'Jadwal_matakuliahController@tambah');
+
